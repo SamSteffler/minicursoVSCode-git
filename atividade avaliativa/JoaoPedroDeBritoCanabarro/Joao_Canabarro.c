c0271b7f0d64c56214a3c26e0965d8c3de6d465f
jphumble <joaopedrocanabarro20@gmail.com>
2026-04-04 12:48:03 -0300

    Atividade Avaliativa- Joao_Canabarro

---
+++ atividade avaliativa/JoaoPedroDeBritoCanabarro/Joao_Canabarro.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    float x=6, y=7, media, soma, dif, resto;
+    soma=x+y;
+    dif=y-x;
+    media= (x+7)/2;
+    resto= 7%6;
+    printf("%f\n%f\n%f\n%f", soma, dif, media, resto);
+    
+}
