eb0fe766ed5267554cf89f104de4653e9296cd2d
sangustavo <guustavo.olinda@gmail.com>
2026-04-05 22:11:39 -0300

    Atividade Avaliativa - Gustavo Olinda Atividade Avaliativa - Gustavo Olinda

---
+++ atividade avaliativa/gustavo_olinda_avaliacao/gustavo_olinda.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Erro: Digite dois numeros. Ex: %s 10 5\n", argv[0]);
+        return 1;
+    }
+    int n1 = atoi(argv[1]);
+    int n2 = atoi(argv[2]);
+
+    int maior, menor;
+    if (n1 > n2) {
+        maior = n1;
+        menor = n2;
+    } else {
+        maior = n2;
+        menor = n1;
+    }
+
+    printf("Soma: %d\n", n1 + n2);
+    printf("Diferenca: %d\n", maior - menor);
+    printf("Media: %.1f\n", (n1 + n2) / 2.0); 
+
+    if (menor != 0) {
+        printf("Resto da divisao: %d\n", maior % menor);
+    } else {
+        printf("Resto da divisao: Nao e possivel dividir por zero.\n");
+    }
+
+    return 0;
+}
